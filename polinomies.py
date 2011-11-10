10736b2bf0f93279712ffacd52d63e9d6f5285ae
mattgaviota <mattgaviota@gmail.com>
2011-11-10 12:35:28 -0300

    agregadas nuevas funcionalidades

--- polinomies.py
+++ polinomies.py
@@ -3,37 +3,101 @@
 
 # imports
 
-# Functions
+# Clases
 
-class Polynomies():
 
-    def __init__(self, coeficientes):
-        self.coeficientes = coeficientes
-    
+class Polinomio():
+
+    def __init__(self, coeficientes=[]):
+        """Inicializa los coeficientes del polinomio.
+        Es en orden ascendente, es decir [A0, A1, A2,...,An]"""
+        self.coeficientes = coeficientes[:]
+
     def derivar(self):
-        self.derivada = Polynomies([x*(i+1) for i,x in
+        """Deriva el polinomio"""
+        self.derivada = Polinomio([x * (i + 1) for i, x in
             enumerate(self.coeficientes[1:])])
 
     def get_derivada(self):
+        """Retorna la derivada del polinomio como otro polinomio"""
         self.derivar()
         return self.derivada
 
     def integrar(self):
-        self.integral = Polynomies([0] + [float(x)/(i+1)
+        """Integra el polinomio"""
+        self.integral = Polinomio([0] + [float(x) / (i + 1)
             for i, x in enumerate(self.coeficientes)])
 
     def get_integral(self):
+        """Retorna la integral del polinomio como otro polinomio"""
         self.integrar()
         return self.integral
 
-    def evaluar(self,valor):
-        return sum([x*valor**i for i, x in enumerate(self.coeficientes)])
+    def evaluar(self, valor):
+        """Metodo para evaluear el polinomio"""
+        return sum([x * valor ** i for i, x in enumerate(self.coeficientes)])
+
+    def __add__(self, other):
+        """Sobrecarga de la suma.
+        Se asume que other también es un polinomio"""
+        r = [a + b for a, b in zip(self.coeficientes, other.coeficientes)]
+        if len(self.coeficientes) < len(other.coeficientes):
+            r += other.coeficientes[len(self.coeficientes):]
+        else:
+            r += self.coeficientes[len(other.coeficientes):]
+        return Polinomio(r)
+
+    def __sub__(self, other):
+        """Sobrecarga de la resta.
+        Se asume que other también es un polinomio"""
+        r = [a - b for a, b in zip(self.coeficientes, other.coeficientes)]
+        if len(self.coeficientes) < len(other.coeficientes):
+            r += [-1 * v for v in other.coeficientes[len(self.coeficientes):]]
+        else:
+            r += self.coeficientes[len(other.coeficientes):]
+        return Polinomio(r)
+
+    def __mul__(self, other):
+        """Sobrecarga del producto.
+        Se asume que other también es un polinomio."""
+        try:
+            r = [0] * (len(self.coeficientes) + len(other.coeficientes) - 1)
+            for sgrado, scoef in enumerate(self.coeficientes):
+                for ogrado, ocoef in enumerate(other.coeficientes):
+                    r[sgrado + ogrado] += scoef * ocoef
+        except AttributeError:
+            r = [other * coef for coef in self.coeficientes]
+        return Polinomio(r)
+
+    def __eq__(self, other):
+        """Comparación de igualdad.
+        Se asume que other también es un polinomio."""
+        for a, b in zip(self.coeficientes, other.coeficientes):
+            if a != b:
+                return False
+        for c in self.coeficientes[len(other.coeficientes):]:
+            if c != 0:
+                return False
+        for c in other.coeficientes[len(self.coeficientes):]:
+            if c != 0:
+                return False
+        return True
+
+    def __ne__(self, other):
+        """Comparación de desigualdad implementada
+        como complementaria de la de igualdad."""
+        return not self.__eq__(other)
+
+    def __call__(self, x):
+        """Evaluación en un punto con notación funcional."""
+        return self.evaluar(x)
 
     def __str__(self):
+        """Metodo para mostrar el polinomio"""
         poly = []
         polytoshow = ''
         coef = self.coeficientes
-        for i,x in enumerate(coef):
+        for i, x in enumerate(coef):
             if x and x != 1:
                 if i == 1:
                     elem = '%sX' % (x,)
@@ -57,17 +121,3 @@ class Polynomies():
             else:
                 polytoshow = polytoshow + elem + '+ '
         return polytoshow[:-2]
-
-
-def main():
-    polinomio = Polynomies([2,-3,1])
-    print 'polinomio: ', polinomio
-    der = polinomio.get_derivada()
-    print 'derivada: ', der
-    inte = polinomio.get_integral()
-    print 'integral: ', inte
-    valor = raw_input('ingrese valor a evaluar: ')
-    print 'polinomio evaluado en %s: %s' % (valor, polinomio.evaluar(float(valor)))
-
-if __name__ == '__main__':
-    main()
